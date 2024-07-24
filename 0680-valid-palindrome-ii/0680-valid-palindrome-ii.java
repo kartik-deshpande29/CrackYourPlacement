4ed23a4189220c90dba391c9012cab62c2615337
Kartik Achyut Deshpande <90626630+kartik-deshpande29@users.noreply.github.com>
2024-07-24 18:43:11 +0530

    Time: 4 ms (99.58%), Space: 45.4 MB (23.34%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -12,6 +12,13 @@ class Solution {
     public boolean validPalindrome(String s) {
         int left = 0;
         int right = s.length() - 1;
+        
+        // The loop continues as long as left is less than right.
+        // If the characters at the left and right pointers are not equal, we have a mismatch.
+        // In case of a mismatch, we check two possibilities:
+        // Removing the character at the left pointer.
+        // Removing the character at the right pointer.
+        // If either of these resulting substrings is a palindrome, we return true.
 
         while (left < right) {
             if (s.charAt(left) != s.charAt(right)) {
