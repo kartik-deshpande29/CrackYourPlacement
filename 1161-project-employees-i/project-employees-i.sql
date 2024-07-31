1fdbf1022abed841f591e831ba8b416e50d42afc
Kartik Achyut Deshpande <90626630+kartik-deshpande29@users.noreply.github.com>
2024-07-31 13:59:15 +0530

    Time: 1825 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1161-project-employees-i/project-employees-i.sql
@@ -0,0 +1,6 @@
+select p.project_id, ROUND(SUM(e.experience_years)/COUNT(p.project_id),2) 
+as average_years
+from Project p
+left join Employee e
+on p.employee_id = e.employee_id
+group by p.project_id;
