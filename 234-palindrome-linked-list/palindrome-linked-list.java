7a6e5526c99be9d28391bbec1734c79496f971f6
Kartik Achyut Deshpande <90626630+kartik-deshpande29@users.noreply.github.com>
2024-08-03 14:54:23 +0530

    Time: 12 ms (21.13%) | Memory: 57.1 MB (86.04%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null) return false;
+        List<Integer> list = new ArrayList<>();
+        List<Integer> rev = new ArrayList<>();
+
+        ListNode temp = head;
+        while (temp != null) {
+            list.add(temp.val);
+            temp = temp.next;
+        }
+
+        ListNode current = head;
+        ListNode prev = null;
+        ListNode Next = null;
+
+        while (current != null) {
+            Next = current.next;
+            current.next = prev;
+            prev = current;
+            current = Next;
+        }
+        temp = prev;
+        int size = 0;
+        while (temp != null) {
+            size++;
+            rev.add(temp.val);
+            temp = temp.next;
+        }
+
+        for (int i = 0; i < size; i++) {
+            if (list.get(i) != rev.get(i)) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+}
