289e8a8e0f1d7c555c5409a0e944670d229e8cfd
Kartik Achyut Deshpande <90626630+kartik-deshpande29@users.noreply.github.com>
2024-08-07 19:05:37 +0530

    Time: 9 ms (92.20%) | Memory: 57.1 MB (27.68%) - LeetSync

---
+++ 148-sort-list/sort-list.java
@@ -0,0 +1,59 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+
+        ListNode dummy = new ListNode(-1);
+        ListNode temp = dummy;
+        ListNode t1 = list1;
+        ListNode t2 = list2;
+
+        while (t1 != null && t2 != null) {
+            if (t1.val <= t2.val) {
+                temp.next = t1;
+                t1 = t1.next;
+                temp = temp.next;
+            }
+            else {
+                temp.next = t2;
+                t2 = t2.next;
+                temp = temp.next;
+            }
+        }
+        if (t1 == null) temp.next = t2;
+        else temp.next = t1;
+        
+        return dummy.next;
+    }
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null)  return head;
+        ListNode firstHalf = head;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode secondHalf = slow.next;
+        slow.next = null;
+
+        firstHalf = sortList(firstHalf);
+        secondHalf = sortList(secondHalf);
+
+        ListNode res = mergeTwoLists(firstHalf, secondHalf);
+        return res;
+        
+
+    }
+}
